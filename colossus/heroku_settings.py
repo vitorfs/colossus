7064783200be7c58297191a291087035165abc74
Vitor Freitas <vitorfs@gmail.com>
2018-08-17 15:52:30 +0300

    Update heroku one click deploy

--- colossus/heroku_settings.py
+++ colossus/heroku_settings.py
@@ -7,3 +7,14 @@ security_middleware_index = MIDDLEWARE.index('django.middleware.security.Securit
 MIDDLEWARE.insert(security_middleware_index + 1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CELERY_BROKER_READ_URL = config('RABBITMQ_BIGWIG_RX_URL')
+CELERY_BROKER_WRITE_URL = config('RABBITMQ_BIGWIG_TX_URL')
+
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='webmaster@localhost')
+
+
+EMAIL_HOST = config('MAILGUN_SMTP_SERVER')
+EMAIL_PORT = config('MAILGUN_SMTP_PORT', cast=int)
+EMAIL_HOST_USER = config('MAILGUN_SMTP_LOGIN')
+EMAIL_HOST_PASSWORD = config('MAILGUN_SMTP_PASSWORD')
