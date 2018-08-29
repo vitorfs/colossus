8759a21e69b81aacf0f6fe8a9a8f7e5ceba16293
Vitor Freitas <vitorfs@gmail.com>
2018-08-29 23:21:17 +0300

    Fix notification test cases

--- colossus/apps/notifications/tests/factories.py
+++ colossus/apps/notifications/tests/factories.py
@@ -1,6 +1,7 @@
 import factory
 
 from colossus.apps.accounts.tests.factories import UserFactory
+from colossus.apps.lists.tests.factories import MailingListFactory
 from colossus.apps.notifications.constants import Actions
 from colossus.apps.notifications.models import Notification
 
@@ -8,6 +9,13 @@ from colossus.apps.notifications.models import Notification
 class NotificationFactory(factory.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
     action = Actions.IMPORT_COMPLETED
+    text = '{"mailing_list_id": 999, "created": 0, "updated": 0, "ignored": 0}'
 
     class Meta:
         model = Notification
+
+    @factory.post_generation
+    def create_mailing_list(self, create, extracted, **kwargs):
+        if not create:
+            return
+        return MailingListFactory(id=999, name='Test List')
