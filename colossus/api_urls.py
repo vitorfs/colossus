5f8d668d1b98f5add8123794a1802b82381560eb
Vitor Freitas <vitorfs@gmail.com>
2019-01-31 15:16:54 +0200

    Fix pep8

--- colossus/api_urls.py
+++ colossus/api_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import include, path
 
 app_name = 'api'
 
--- colossus/apps/lists/api/urls.py
+++ colossus/apps/lists/api/urls.py
@@ -1,5 +1,3 @@
-from django.urls import path
-
 from rest_framework.routers import DefaultRouter
 
 from . import views
